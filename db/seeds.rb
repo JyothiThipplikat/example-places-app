38f474a9539b7a2ece1bcbd2e9c5ad821ff1df36
“JyothiThipplikat” <lilmuthu4life21@gmail.com>
2018-10-18 21:32:38 -0500

    added axios

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+place = Place.create(name: "Jack's Pizza", address: "400 North Wells Chicago, IL")
+
+place = Place.create(name: "Spa for days", address: "456 Greenview Niles, IL")
+
+place = Place.create(name: "Rina's Boutique", address: "900 June Glenview, IL")
+
+place = Place.create(name: "Laser Tag", address: "780 Kindle Niles, IL")
+
+place = Place.create(name: "Restaurants", address: "5679 Waterfall Niles, IL")
